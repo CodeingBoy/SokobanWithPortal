726d93b0034f57069dd79df38ecef96f183bf08b
CodeingBoy <codeingboy@gmail.com>
2016-07-26 17:38:22 +0800

    引入设置类

---
+++ src/sokoban/utils/Settings.java
@@ -0,0 +1,13 @@
+package sokoban.utils;
+
+/**
+ * Created by CodeingBoy on 2016-7-9-0009.
+ */
+public class Settings {
+
+    public static boolean isSettingExist() {
+
+        return false;
+    }
+
+}
