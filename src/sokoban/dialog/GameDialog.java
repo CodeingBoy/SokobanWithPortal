a62c6b7fc5669e8114e0f17bc707dc1b9501af78
CodeingBoy <codeingboy@gmail.com>
2016-07-28 19:55:39 +0800

    引入速度变量

--- src/sokoban/dialog/GameDialog.java
+++ src/sokoban/dialog/GameDialog.java
@@ -1,6 +1,8 @@
 package sokoban.dialog;
 
+import sokoban.game.engine.graphics.CoordinateSystemShower;
 import sokoban.game.engine.graphics.Matrix3x3f;
+import sokoban.game.engine.graphics.ScreenMappingTool;
 import sokoban.game.engine.graphics.Vector2f;
 import sokoban.game.engine.input.KeyboardInput;
 import sokoban.game.engine.input.MouseInput;
@@ -11,6 +13,8 @@ import sokoban.utils.Log;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
  */
 public class GameDialog extends JFrame implements Runnable {
     private FrameRateCalculator frameRate = null;
+    private Canvas canvas;
     private BufferStrategy bufferStrategy;
     private Thread renderThread;
     private boolean rendering = false;
@@ -29,12 +34,15 @@ public class GameDialog extends JFrame implements Runnable {
     private float angel;
     private ArrayList<Point> points = new ArrayList();
     private float earthRot, moonRot;
+    private ScreenMappingTool screenMappingTool;
 
     public GameDialog() throws HeadlessException {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(500, 500);
         setTitle("GameDialog");
         setIgnoreRepaint(true); // active rendering, need not passive repaint 主动渲染，无须被动渲染
+        setBackground(Color.gray);
+        getContentPane().setBackground(Color.blue);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -83,13 +91,23 @@ public class GameDialog extends JFrame implements Runnable {
     }
 
     public void createAndShowGUI() {
-        Canvas canvas = new Canvas();
+        canvas = new Canvas();
         canvas.setBackground(Color.black);
         add(canvas);
         setVisible(true);
 
         canvas.createBufferStrategy(2);
         bufferStrategy = canvas.getBufferStrategy();
+
+        screenMappingTool = new ScreenMappingTool(5, 5, canvas);
+
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                screenMappingTool = new ScreenMappingTool(5, 5, canvas);
+            }
+        });
+        // addComponentListener(new ResizingHandler(canvas, getContentPane(), 4, 3, 0));
 
         KeyboardInput keyboardInput = new KeyboardInput();
         MouseInput mouseInput = new MouseInput();
@@ -118,26 +136,35 @@ public class GameDialog extends JFrame implements Runnable {
     public void run() {
         rendering = true;
         frameRate.initialize();
+
+        long curTime = System.nanoTime();
+        long lastTime = curTime;
+        double nsPerSec;
+
         while (rendering) {
+            curTime = System.nanoTime();
+            nsPerSec = curTime - lastTime;
+            lastTime = curTime;
+
             keyboardInputHandler.poll();
             keyboardInputHandler.processInput();
             mouseInputHandler.poll();
             mouseInputHandler.processInput();
-            renderLoop();
+            renderLoop(nsPerSec / 1.0E9);
             try {
-                Thread.sleep(10);
+                Thread.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void renderLoop() {
+    private void renderLoop(double delta) {
         do {
             do {
                 Graphics g = bufferStrategy.getDrawGraphics();
                 g.clearRect(0, 0, getWidth(), getHeight());
-                render(g);
+                render(g, delta);
 
                 if (g != null) {
                     g.dispose();
@@ -147,17 +174,25 @@ public class GameDialog extends JFrame implements Runnable {
         } while (bufferStrategy.contentsLost());
     }
 
-    private void render(Graphics g) {
+    private void render(Graphics g, double delta) {
         if (frameRate != null) {
             frameRate.calculate();
             g.setColor(Color.white);
             g.drawString(frameRate.getLatestFrameRateString(), 50, 50);
         }
 
+        // CoordinateSystemShower.drawWorldXAxis(g, 0, 300, 50, canvas.getHeight(), true, 20);
+        // CoordinateSystemShower.drawWorldYAxis(g, 0, 300, 50, canvas.getWidth(), true, 20);
+        // System.out.println("in render " + canvas.getSize());
+
+        CoordinateSystemShower.drawScreenXAxis(g, 0, 300, 50, canvas.getHeight(), screenMappingTool, 0);
+        CoordinateSystemShower.drawScreenYAxis(g, 0, 300, 50, canvas.getWidth(), screenMappingTool, 0);
+
         // calc the square pos
         Matrix3x3f squMat = Matrix3x3f.translate(100, 100);
-        squMat = squMat.mul(squMat.rotate(angel));
-        squMat = squMat.mul(squMat.translate(200, 200));
+        squMat = squMat.mul(Matrix3x3f.rotate(angel));
+        squMat = squMat.mul(Matrix3x3f.translate(200, 200));
+        squMat = screenMappingTool.getScreenMatrix(squMat);
         angel += Math.toRadians(0.03);
         Vector2f squVec = squMat.mul(new Vector2f(0, 0, 1));
 
@@ -166,8 +201,10 @@ public class GameDialog extends JFrame implements Runnable {
         g.fillRect((int) squVec.x - 10, (int) squVec.y - 10, 20, 20);
 
         // calc the sun pos
-        Matrix3x3f sunMat = Matrix3x3f.translate(getWidth() / 2, getHeight() / 2);
+        Matrix3x3f sunMat = Matrix3x3f.translate(screenMappingTool.worldToScreen(new Point(0, 0)));
         Vector2f sunVec = sunMat.toVector();
+
+        // System.out.println(screenMappingTool.worldToScreen(new Point(0, 0)));
 
         // draw the sun
         g.setColor(Color.yellow);
@@ -175,13 +212,14 @@ public class GameDialog extends JFrame implements Runnable {
 
         // draw the orbit
         g.setColor(Color.white);
-        g.drawOval((int) sunVec.x - getWidth() / 4, (int) sunVec.y - getWidth() / 4, getWidth() / 2, getWidth() / 2);
+        g.drawOval((int) sunVec.x - canvas.getWidth() / 4, (int) sunVec.y - canvas.getWidth() / 4,
+                canvas.getWidth() / 2, canvas.getWidth() / 2);
 
         // calc the earth pos
-        Matrix3x3f earthMat = Matrix3x3f.translate(getWidth() / 4, 0);
-        earthMat = earthMat.mul(earthMat.rotate(earthRot));
+        Matrix3x3f earthMat = Matrix3x3f.translate(canvas.getWidth() / 4, 0);
+        earthMat = earthMat.mul(Matrix3x3f.rotate(earthRot));
         earthMat = earthMat.mul(sunMat);
-        earthRot += Math.toRadians(0.5);
+        earthRot += Math.toRadians(180) * delta;
         Vector2f earthVec = earthMat.toVector();
 
         // draw the earth
