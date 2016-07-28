f0532f20e57df0c56fb8f6a0dc334e904660bb6b
CodeingBoy <codeingboy@gmail.com>
2016-07-28 22:16:02 +0800

    小调整~

--- src/sokoban/game/engine/GameWindow.java
+++ src/sokoban/game/engine/GameWindow.java
@@ -1,9 +1,5 @@
 package sokoban.game.engine;
 
-import sokoban.dialog.GameDialog;
-import sokoban.dialog.LogDialog;
-import sokoban.game.utils.FrameRateCalculator;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
@@ -12,64 +8,73 @@ import java.awt.event.WindowEvent;
 /**
  * Created by CodeingBoy on 2016-7-28-0028.
  */
-public abstract class GameWindow extends JFrame {
+public class GameWindow extends JFrame {
     private Scene scene;
 
     public GameWindow(Dimension size, String title, Scene scene) throws HeadlessException {
         if (scene != null) {
             this.scene = scene;
+            scene.setWindow(this);
             scene.onPrepare();
         }
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(size);
-        setTitle("GameDialog");
+        setTitle(title);
         setIgnoreRepaint(true); // active rendering, need not passive repaint 主动渲染，无须被动渲染
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
-                scene.onDestroy();
+                scene.destroy();
+                System.exit(0);
             }
         });
     }
-    public static void main(String[] args) {
-        GameDialog dialog = new GameDialog();
-        FrameRateCalculator frameRate = new FrameRateCalculator();
-        frameRate.setShouldLog(true);
-        dialog.setFrameRate(frameRate);
+    // public static void main(String[] args) {
+    //     GameDialog dialog = new GameDialog();
+    //     FrameRateCalculator frameRate = new FrameRateCalculator();
+    //     frameRate.setShouldLog(true);
+    //     dialog.setFrameRate(frameRate);
+    //
+    //     LogDialog logDialog = LogDialog.getInstance();
+    //     logDialog.setVisible(true);
+    //
+    //     SwingUtilities.invokeLater(new Runnable() {
+    //         @Override
+    //         public void run() {
+    //             dialog.createAndShowGUI();
+    //             dialog.setVisible(true);
+    //         }
+    //     });
+    // }
 
-        LogDialog logDialog = LogDialog.getInstance();
-        logDialog.setVisible(true);
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                dialog.createAndShowGUI();
-                dialog.setVisible(true);
-            }
-        });
-    }
-
-    public final JFrame getWindow(){
+    public final JFrame getWindow() {
         return this;
     }
 
     public final void showWindow() {
-        if (scene==null)
+        if (scene == null)
             throw new IllegalStateException("Game window have no scene");
 
         scene.Initialize();
+
+        if (scene.getCanvas() == null) {
+            throw new IllegalArgumentException("have not canvas");
+        }
         add(scene.getCanvas());
 
         setVisible(true);
+
+        scene.startRendering();
     }
 
 
-
-    public final void switchScene(Scene newScene){
+    public final void switchScene(Scene newScene) {
         if (isVisible()) {
-            scene.onDestroy();
+            scene.destroy();
+            scene.setWindow(null);
+            newScene.setWindow(this);
             newScene.onPrepare();
             newScene.onInitialize();
         }
--- src/sokoban/game/engine/Scene.java
+++ src/sokoban/game/engine/Scene.java
@@ -4,20 +4,21 @@ package sokoban.game.engine;
  * Created by CodeingBoy on 2016-7-28-0028.
  */
 
+import sokoban.utils.Log;
+
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
-/**
- * Created by CodeingBoy on 2016-7-10-0010.
- */
 public abstract class Scene implements Runnable {
     protected Canvas canvas;
-    private BufferStrategy bufferStrategy;
+    protected BufferStrategy bufferStrategy;
     private Thread renderThread;
     private boolean rendering = false;
+    protected GameWindow window;
 
     /**
      * GameWindow 初始化或切换场景时会调用此函数 可以重写该函数以更改游戏窗口的部分属性
+     *
      * @see sokoban.game.engine.GameWindow#GameWindow(Dimension, String, Scene)
      */
     public abstract void onPrepare();
@@ -25,12 +26,14 @@ public abstract class Scene implements Runnable {
     /**
      * 即将开始渲染之前会调用此函数 请重写该函数以确保您的场景准备好进行渲染<br/>
      * 该函数将在游戏窗口开始渲染前被调用
+     *
      * @see #onInitialize()
      */
     public abstract void onInitialize();
 
     /**
      * 场景被切换或游戏窗口关闭时会调用此函数 请重写该函数以确保您的资源被正确释放
+     *
      * @see sokoban.game.engine.GameWindow#switchScene(Scene)
      * @see sokoban.game.engine.GameWindow#GameWindow(Dimension, String, Scene)
      */
@@ -55,26 +58,52 @@ public abstract class Scene implements Runnable {
      * 初始化场景 为即将渲染做准备<br/>
      * 将由 GameWindow 类中的 showWindow 方法进行调用 请勿直接调用此函数<br/>
      * 欲自定义即将显示中的过程 请重写 onInitialize() 函数
+     *
+     * @throws IllegalArgumentException 所需的参数不足 请补全参数
      * @see #onInitialize
-     * @exception IllegalArgumentException 所需的参数不足 请补全参数
      */
     public final void Initialize() throws IllegalArgumentException {
-        if (canvas == null)
-            throw new IllegalArgumentException("have not canvas");
-        // add(canvas); // will be added by showWindow() in class GameWindow
+        onInitialize();
 
+        // add(canvas); // will be added by showWindow() in class GameWindow
+    }
+
+    public void setWindow(GameWindow window) {
+        this.window = window;
+    }
+
+    public final void startRendering() {
         canvas.createBufferStrategy(2);
         bufferStrategy = canvas.getBufferStrategy();
-
-        onInitialize();
 
         renderThread = new Thread(this);
         renderThread.start();
     }
 
+    public final void destroy() {
+        Log.d("stopping rendering...");
+        rendering = false;
+        try {
+            renderThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d("render thread stopped!");
+
+        Log.d("Exiting...");
+        // TODO: Wait for Log dialog
+        // try {
+        //     LogDialog.getInstance().wait();
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        onDestroy();
+    }
+
     /**
      * 返回画布 canvas<br/>
      * return var canvas
+     *
      * @return 返回画布 canvas 的引用<br/>
      * return reference to canvas
      */
@@ -85,6 +114,7 @@ public abstract class Scene implements Runnable {
     /**
      * 游戏渲染线程函数<br/>
      * 您可以通过重写 onPrepareRendering(), onRendering(), onExitingRendering() 来更改场景在渲染线程中的行为
+     *
      * @see #onPrepareRendering()
      * @see #onRendering()
      * @see #onExitingRendering()
@@ -99,6 +129,14 @@ public abstract class Scene implements Runnable {
         }
 
         onExitingRendering();
+    }
+
+    public final int getWidth() {
+        return canvas.getWidth();
+    }
+
+    public final int getHeight() {
+        return canvas.getHeight();
     }
 
 }
