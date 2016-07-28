88b60a5aa9cdf2ad9387ca29d978ed7523775f05
CodeingBoy <codeingboy@gmail.com>
2016-07-28 19:57:12 +0800

    变量更名等调整

--- src/sokoban/game/engine/graphics/ScreenMappingTool.java
+++ src/sokoban/game/engine/graphics/ScreenMappingTool.java
@@ -7,22 +7,22 @@ import java.awt.*;
  */
 public class ScreenMappingTool {
     private float worldWidth, worldHeight;
-    private float screenWidth, screenHeight;
+    private float canvasWidth, canvasHeight;
     private Matrix3x3f mappingMatrix;
 
     public ScreenMappingTool(float worldWidth, float worldHeight, Canvas canvas) {
         this.worldWidth = worldWidth;
         this.worldHeight = worldHeight;
-        screenWidth = canvas.getWidth() - 1;
-        screenHeight = canvas.getHeight() - 1;
+        canvasWidth = canvas.getWidth() - 1;
+        canvasHeight = canvas.getHeight() - 1;
         calcMappingMatrix();
     }
 
-    public ScreenMappingTool(float worldWidth, float worldHeight, float screenWidth, float screenHeight) {
+    public ScreenMappingTool(float worldWidth, float worldHeight, float canvasWidth, float canvasHeight) {
         this.worldWidth = worldWidth;
         this.worldHeight = worldHeight;
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
         calcMappingMatrix();
     }
 
@@ -30,25 +30,17 @@ public class ScreenMappingTool {
         return worldWidth;
     }
 
-    public void setWorldWidth(float worldWidth) {
-        this.worldWidth = worldWidth;
-    }
-
     public float getWorldHeight() {
         return worldHeight;
     }
 
-    public void setWorldHeight(float worldHeight) {
-        this.worldHeight = worldHeight;
-    }
-
     private void calcMappingMatrix() {
         // calc scale ratio
-        float scale_x = screenWidth / worldWidth;
-        float scale_y = screenHeight / worldHeight;
+        float scale_x = canvasWidth / worldWidth;
+        float scale_y = canvasHeight / worldHeight;
         // calc translate
-        float translate_x = screenWidth / 2;
-        float translate_y = screenWidth / 2;
+        float translate_x = canvasWidth / 2;
+        float translate_y = canvasHeight / 2;
         // calc matrix
         mappingMatrix = Matrix3x3f.scale(scale_x, -scale_y).mul(Matrix3x3f.translate(translate_x, translate_y));
     }
@@ -57,8 +49,8 @@ public class ScreenMappingTool {
         return mappingMatrix.mul(matrix);
     }
 
-    public Point getScreenPoint(Point worldPoint) {
-        Vector2f v = getScreenMatrix(Matrix3x3f.translate(new Vector2f(worldPoint.x, worldPoint.y, 1))).toVector();
+    public Point worldToScreen(Point worldPoint) {
+        Vector2f v = getScreenMatrix(Matrix3x3f.translate(new Point(worldPoint.x, worldPoint.y))).toVector();
         return new Point((int) v.x, (int) v.y);
     }
 
