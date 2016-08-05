7b1ddce103983b22f27d1f918f87cd1067b6d408
CodeingBoy <codeingboy@gmail.com>
2016-08-05 14:43:15 +0800

    地图类及地图解析器

--- src/sokoban/map/GameMap.java
+++ src/sokoban/map/GameMap.java
@@ -1,18 +1,70 @@
 package sokoban.map;
 
+import sokoban.game.engine.graphics.ScreenMappingTool;
 import sokoban.game.engine.graphics.shapes.Drawable;
+import sokoban.map.objects.MapObject;
 
 import java.awt.*;
+import java.util.Map;
 
 /**
  * Created by CodeingBoy on 2016-8-4-0004.
  */
 public class GameMap implements Drawable {
-    private Drawable[][] mapObjs;
+    private final static int PICWIDTH = 50, PICHEIGHT = 50;
+    private MapObject[][] mapObjs;
+    private int mapWidth, mapHeight;
+    private String MapName;
+    private Map<String, String> MapProperty;
+    private ScreenMappingTool screenMappingTool;
+
+    public GameMap(MapObject[][] mapObjs, int mapWidth, int mapHeight, String mapName, Map<String, String> mapProperty) {
+        this.mapObjs = mapObjs;
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+        MapName = mapName;
+        MapProperty = mapProperty;
+    }
+
+    public GameMap(MapObject[][] mapObjs, Map<String, String> mapProperty) {
+        this.mapObjs = mapObjs;
+        this.mapWidth = Integer.parseInt(mapProperty.get("MapWidth"));
+        this.mapHeight = Integer.parseInt(mapProperty.get("MapHeight"));
+        MapName = mapProperty.get("MapName");
+        MapProperty = mapProperty;
+    }
+
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
+
+    public void setScreenMappingTool(ScreenMappingTool screenMappingTool) {
+        this.screenMappingTool = screenMappingTool;
+    }
+
+    public String getProperty(String name) {
+        return MapProperty.get(name);
+    }
+
+    public void updateMapScreenPos(){
+        for (int i = 0; i < mapHeight; i++) {
+            for (int j = 0; j < mapWidth; j++) {
+                mapObjs[i][j].worldToScreen(screenMappingTool);
+            }
+        }
+    }
 
     @Override
     public void draw(Graphics g, double delta) {
-
+        for (int i = 0; i < mapHeight; i++) {
+            for (int j = 0; j < mapWidth; j++) {
+                mapObjs[i][j].draw(g, delta);
+            }
+        }
     }
 
     @Override
--- src/sokoban/map/MapParser.java
+++ src/sokoban/map/MapParser.java
@@ -1,6 +1,13 @@
 package sokoban.map;
 
+import sokoban.map.objects.Floor;
+import sokoban.map.objects.MapObject;
+import sokoban.map.objects.Wall;
+
+import java.awt.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by CodeingBoy on 2016-8-4-0004.
@@ -10,6 +17,63 @@ public class MapParser {
     public static GameMap parseMapFile(File mapFile) throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(mapFile)));
 
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            while (bufferedReader.ready())
+                stringBuffer.append(bufferedReader.readLine() + "\r\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String part[] = stringBuffer.toString().trim().split("\r\n\r\n");
+
+        // 读入地图基本信息
+        String basicInf[] = part[0].split("\r\n");
+        Map<String, String> inf = new HashMap<>();
+        for (String s : basicInf) {
+            String[] line = s.split(":");
+            inf.put(line[0], line[1]);
+        }
+
+        int mapWidth = Integer.parseInt(inf.get("MapWidth"));
+        int mapHeight = Integer.parseInt(inf.get("MapHeight"));
+
+        int XOffset = 0;
+        int YOffset = 0;
+        if (inf.get("Offset") != null) {
+            XOffset = Integer.parseInt(inf.get("Offset").split(",")[0]);
+            YOffset = -Integer.parseInt(inf.get("Offset").split(",")[1]);
+        }
+
+        MapObject[][] mapObjects = new MapObject[mapHeight][mapWidth];
+
+        // 读入地图数据
+        String mapRow[] = part[1].split("\r\n");
+        for (int row = 0; row < mapRow.length; row++) {
+            String[] mapCol = mapRow[row].split(",");
+            for (int col = 0; col < mapCol.length; col++) {
+                Point pos = new Point(XOffset + col, YOffset + row);
+                switch (Integer.parseInt(mapCol[col])) {
+                    case 1: // floor
+                        mapObjects[row][col] = new Floor(pos);
+                        break;
+                    // case 2:
+                    //     mapObjects[row][col] = new Floor();
+                    //     break;
+                    // case 3:
+                    //     mapObjects[row][col] = new Floor();
+                    //     break;
+                    case 0:
+                        mapObjects[row][col] = new Wall(pos);
+                        break;
+                    default:
+                        mapObjects[row][col] = null;
+                        break;
+                }
+            }
+        }
+
+        return new GameMap(mapObjects, mapWidth, mapHeight, inf.get("MapName"), inf);
 
     }
 
