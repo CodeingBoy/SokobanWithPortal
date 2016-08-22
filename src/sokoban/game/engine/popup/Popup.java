70749a62d76abd73bf1557e5d70066046913b823
CodeingBoy <codeingboy@gmail.com>
2016-08-22 20:31:36 +0800

    新建Popup类

--- src/sokoban/game/engine/popup/Popup.java
+++ src/sokoban/game/engine/popup/Popup.java
@@ -1,20 +1,32 @@
 package sokoban.game.engine.popup;
 
 import sokoban.game.engine.graphics.shapes.Drawable;
+import sokoban.game.engine.scenes.Scene;
 
 import java.awt.*;
 
-/**
- * Created by CodeingBoy on 2016-8-22-0022.
- */
-public class Popup implements Drawable {
+public abstract class Popup implements Drawable {
+    private Scene scene;
+    private Color backgroundColor;
+
     @Override
     public void draw(Graphics g, double delta) {
+        g.setColor(backgroundColor);
+        g.fillRect(0, 0, scene.getWidth(), scene.getHeight());
 
+        render(g, delta);
     }
 
     @Override
     public void fill(Graphics g, double delta) {
         draw(g, delta);
     }
+
+    /**
+     * 渲染函数，请重写该函数以便渲染弹出窗口画面
+     *
+     * @param g     图形对象
+     * @param delta 时间增量
+     */
+    public abstract void render(Graphics g, double delta);
 }
