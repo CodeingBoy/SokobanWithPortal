339ac433d4a21d14e77003d9e53ed45daee55ce9
CodeingBoy <codeingboy@gmail.com>
2016-08-23 13:10:21 +0800

    MessageBoxPopup增强

---
+++ src/sokoban/game/engine/popups/MessageBoxPopup.java
@@ -0,0 +1,22 @@
+package sokoban.game.engine.popups;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by CodeingBoy on 2016-8-22-0022.
+ */
+public class MessageBoxPopup extends Popup {
+    private boolean haveAnimation = true;
+    private String message = null;
+
+
+    @Override
+    public void render(Graphics g, double delta) {
+
+    }
+
+    public void addListener(ActionListener actionListener) {
+
+    }
+}
