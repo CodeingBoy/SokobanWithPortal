c6b021b3ebae461cf41d9c57c3f2350154f08571
CodeingBoy <codeingboy@gmail.com>
2016-10-06 23:01:32 +0800

    增加悬停声音播放和点击声音播放

--- src/sokoban/game/engine/graphics/components/Button.java
+++ src/sokoban/game/engine/graphics/components/Button.java
@@ -7,14 +7,21 @@ import sokoban.game.engine.input.handler.Clickable;
 import sokoban.game.utils.TextDrawer;
 import sokoban.utils.Settings;
 
+import javax.sound.sampled.*;
 import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * 按钮类，定义了基于图片的按钮
  */
-public abstract class Button extends Rect implements Clickable, Drawable {
+public abstract class Button extends Rect implements Drawable, Clickable {
     private Image normalImg, hoverImg, pressImg;
     private boolean isClicking = false, isHovering = false;
+    private Clip hoverSound = null, clickSound = null;
+    private boolean isFirstClicking = false, isFirstHovering = false;
 
     @Deprecated
     public Button(Vector2f start, int width, int height, String text, Image normalImg, Image hoverImg, Image pressImg) {
@@ -84,6 +91,11 @@ public abstract class Button extends Rect implements Clickable, Drawable {
 
     @Override
     public void onClick(Point p) {
+        if (clickSound != null) {
+            clickSound.setFramePosition(0);
+            clickSound.start();
+        }
+
         isClicking = true;
     }
 
@@ -92,12 +104,72 @@ public abstract class Button extends Rect implements Clickable, Drawable {
         isHovering = true;
     }
 
+    @Override
+    public void onEnteringHover(Point p) {
+        if (hoverSound != null) {
+            hoverSound.setFramePosition(0);
+            hoverSound.start();
+        }
+    }
+
+    @Override
+    public void onExitingHover(Point p) {
+
+    }
+
     protected boolean isClicking() {
         return isClicking;
     }
 
     protected boolean isHovering() {
         return isHovering;
+    }
+
+    public void setHoverSound(File soundFile) throws IOException {
+        try {
+            hoverSound = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
+        AudioInputStream audioInputStream = readAudioInputStreamFromFile(soundFile);
+        try {
+            hoverSound.open(audioInputStream);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setClickSound(File soundFile) throws IOException {
+        try {
+            clickSound = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
+        AudioInputStream audioInputStream = readAudioInputStreamFromFile(soundFile);
+        try {
+            clickSound.open(audioInputStream);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private AudioInputStream readAudioInputStreamFromFile(File file) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        byte[] bytes = new byte[fileInputStream.available()];
+        fileInputStream.read(bytes);
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        AudioInputStream audioInputStream = null;
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(byteArrayInputStream);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
+        return audioInputStream;
     }
 
     @Override
