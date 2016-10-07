0e3b3e2b6c7d17c8ee74c259aacb2858d7d4e184
CodeingBoy <codeingboy@gmail.com>
2016-10-07 17:21:04 +0800

    添加转换到AWT显示模式函数

--- src/sokoban/game/utils/DisplayMode.java
+++ src/sokoban/game/utils/DisplayMode.java
@@ -55,6 +55,10 @@ public class DisplayMode {
         return refreshRate;
     }
 
+    public java.awt.DisplayMode toAWTDisplayMode() {
+        return new java.awt.DisplayMode(size.width, size.height, bitDepth, refreshRate);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
